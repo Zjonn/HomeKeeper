64e22b9a372bd7a54b577f75f4a649d52fc2a5cf
zjonn <zjonnn@gmail.com>
2021-06-07 00:03:02 +0200

    Refactors widgets

---
+++ lib/widgets/button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CommonMaterialButton extends StatelessWidget {
+  final label;
+  final onPressed;
+
+  CommonMaterialButton(this.label, {this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      minWidth: MediaQuery.of(context).size.width,
+      color: Theme.of(context).primaryColor,
+      onPressed: onPressed,
+      child: Text(
+        label,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+}
+
+class CommonButton extends StatelessWidget {
+  final label;
+  final onPressed;
+
+  CommonButton(this.label, {this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    throw TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(0.0),
+        ),
+        child: Text(label, style: TextStyle(fontWeight: FontWeight.w300)),
+        onPressed: onPressed);
+  }
+}
---
+++ lib/widgets/flushbar.dart
@@ -0,0 +1,23 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CommonFlushbar extends Flushbar {
+  CommonFlushbar(String message, {seconds = 2})
+      : super(
+            message: message,
+            duration: Duration(seconds: seconds),
+            borderRadius: BorderRadius.circular(40),
+            margin: EdgeInsets.only(left: 10, right: 10, bottom: 60));
+
+  var _backgroundColor;
+
+  Color get backgroundColor {
+    return _backgroundColor;
+  }
+
+  Future show(BuildContext context) {
+    _backgroundColor = Theme.of(context).primaryColor.withOpacity(0.5);
+    return super.show(context);
+  }
+}
