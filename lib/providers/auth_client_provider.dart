becca29f5ddfaece08f8b4b662e1f059911de11e
zjonn <zjonnn@gmail.com>
2021-06-12 00:00:31 +0200

    Implement auth_client_provider

--- lib/providers/auth_client_provider.dart
+++ lib/providers/auth_client_provider.dart
@@ -3,28 +3,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:home_keeper/config/client.dart';
 
-enum State { ClientInitialized, ClientUninitialized }
+enum ClientState { Initialized, Uninitialized }
 
-class ClientProvider extends ChangeNotifier {
+class AuthClientProvider extends ChangeNotifier {
   late final ArtemisClient _client;
   late final FlutterSecureStorage _storage;
 
-  var _state = State.ClientUninitialized;
+  var _state = ClientState.Uninitialized;
 
   get state => _state;
 
   get client => _client;
 
-  ClientProvider();
+  AuthClientProvider() {
+    _storage = FlutterSecureStorage();
+  }
 
-  ClientProvider.withMocks(_storage);
+  AuthClientProvider.withMocks(_storage);
 
-  void initialize() async {
-    if (_state == State.ClientUninitialized) {
+  Future<void> initialize() async {
+    if (_state == ClientState.Uninitialized) {
       String token = (await this._storage.read(key: "token"))!;
       _client = initializeClient(token);
 
-      _state = State.ClientInitialized;
+      _state = ClientState.Initialized;
       notifyListeners();
     }
   }
