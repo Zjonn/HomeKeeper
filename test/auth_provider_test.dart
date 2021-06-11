b01dde05b2c70a58e3eab030649338dbc370bfb7
zjonn <zjonnn@gmail.com>
2021-06-11 21:17:02 +0200

    Bump packages versions

--- test/auth_provider_test.dart
+++ test/auth_provider_test.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
 
+import 'package:artemis/client.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:home_keeper/providers/auth_provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 @TestOn("vm")
 import 'package:test/test.dart';
+
+import 'auth_provider_test.mocks.dart';
 
 http.StreamedResponse simpleResponse(String body, {int? status}) {
   final List<int> bytes = utf8.encode(body);
@@ -16,47 +21,41 @@ http.StreamedResponse simpleResponse(String body, {int? status}) {
   return r;
 }
 
-class MockHttpClient extends Mock implements http.Client {}
-
-class MockFlutterSecureStorage extends Mock implements FlutterSecureStorage {}
-
+@GenerateMocks([http.Client, FlutterSecureStorage])
 void main() {
-  // late ArtemisClient artemisClient;
-  // late MockHttpClient mockHttpClient;
-  // late AuthProvider authProvider;
-  // late MockFlutterSecureStorage mockStorage;
+  late ArtemisClient artemisClient;
+  late MockClient mockHttpClient;
+  late AuthProvider authProvider;
+  late MockFlutterSecureStorage mockStorage;
 
-  group(
-    'authProvider',
-    () {
-      setUp(() {
-        // mockHttpClient = MockHttpClient();
-        // mockStorage = MockFlutterSecureStorage();
-        //
-        // artemisClient = ArtemisClient('http://localhost:3001/graphql',
-        //     httpClient: mockHttpClient);
-        //
-        // authProvider = AuthProvider.withMocks(artemisClient, mockStorage);
-      });
+  group('authProvider', () {
+    setUp(() {
+      mockHttpClient = MockClient();
+      mockStorage = MockFlutterSecureStorage();
 
-      // TODO complete this test to actually check something
-      // test(
-      //     'loginUser',
-      // () async {
-      //   when(mockHttpClient.send(http.Request("Test", Uri())))
-      //       .thenAnswer((Invocation a) async {
-      //     return simpleResponse('''{
-      //       "data": {
-      //         "tokenAuth": {
-      //           "token": "token_value",
-      //           "refreshExpiresIn": 1610302338
-      //         }
-      //       }
-      //     } ''');
-      //   });
+      artemisClient = ArtemisClient('http://localhost:3001/graphql',
+          httpClient: mockHttpClient);
 
-      // await authProvider.login("username", "password");
-      // verify(mockStorage.write(key: "token", value: "token_value"));
-    },
-  );
+      authProvider = AuthProvider.withMocks(artemisClient, mockStorage);
+    });
+
+    // TODO complete this test to actually check something
+    test(
+      'loginUser',
+      () async {
+        when(mockHttpClient.send(http.Request("Test", Uri())))
+            .thenAnswer((Invocation a) async => simpleResponse('''{
+            "data": {
+              "tokenAuth": {
+                "token": "token_value",
+                "refreshExpiresIn": 1610302338
+              }
+            }
+          } '''));
+
+        await authProvider.login("username", "password");
+        verify(mockStorage.write(key: "token", value: "token_value"));
+      },
+    );
+  });
 }
