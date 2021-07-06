6ee4af6b777461e855fbf23cfe85ddc2103bd36b
zjonn <zjonnn@gmail.com>
2021-07-06 20:25:12 +0200

    Add basic implementation of task_provider

--- lib/providers/tasks_provider.dart
+++ lib/providers/tasks_provider.dart
@@ -1,18 +1,115 @@
 import 'package:artemis/client.dart';
+import 'package:artemis/schema/graphql_response.dart';
 import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/foundation.dart';
 import 'package:home_keeper/graphql/graphql_api.dart';
 
-class Task {}
+class Task {
+  Task.fromResp(ListTasks$Query$TaskType resp) {}
 
-class TaskInstance {}
+  Task.fromCreateResp(CreateTask$Mutation$CreateTaskPayload$TaskType resp) {}
+}
+
+class TaskInstance {
+  TaskInstance.fromResp(ListTaskInstances$Query$TaskInstanceType resp) {}
+}
+
+class TaskCompletion {
+  TaskCompletion.fromResp(
+      ListTasksCompletions$Query$TaskInstanceCompletionType resp) {}
+}
+
+class CreateResult {
+  bool status;
+
+  GraphQLResponse<CreateTask$Mutation> response;
+
+  CreateResult(this.status, this.response);
+}
 
 class TasksProvider with ChangeNotifier {
   late final ArtemisClient _client;
 
+  List<Task> tasks = [];
+  List<TaskInstance> taskInstances = [];
+  List<TaskCompletion> taskCompletions = [];
+
   TasksProvider(this._client);
 
-  void listUserTasks() {
-    _client.execute(ListUserTeamsInfoQuery());
+  Future<void> updateUserTasks(int teamId) async {
+    GraphQLResponse<ListTasks$Query> response = await _client
+        .execute(ListTasksQuery(variables: ListTasksArguments(teamId: teamId)));
+    assert(!response.hasErrors, response.errors.toString());
+
+    List<Task> tasks_ = [
+      for (var task in response.data!.tasks!) Task.fromResp(task!)
+    ];
+
+    if (listEquals<Task>(tasks, tasks_)) {
+      return;
+    }
+
+    tasks = tasks_;
+    notifyListeners();
+  }
+
+  Future<void> updateUserTaskInstances(int teamId) async {
+    GraphQLResponse<ListTaskInstances$Query> response = await _client.execute(
+        ListTaskInstancesQuery(
+            variables: ListTaskInstancesArguments(teamId: teamId)));
+    assert(!response.hasErrors, response.errors.toString());
+
+    List<TaskInstance> taskInstances_ = [
+      for (var task in response.data!.taskInstances!)
+        TaskInstance.fromResp(task!)
+    ];
+
+    if (listEquals<TaskInstance>(taskInstances, taskInstances_)) {
+      return;
+    }
+
+    taskInstances = taskInstances_;
+    notifyListeners();
+  }
+
+  Future<void> updateUserTaskCompletions(int teamId) async {
+    GraphQLResponse<ListTasksCompletions$Query> response =
+        await _client.execute(ListTasksCompletionsQuery(
+            variables: ListTasksCompletionsArguments(teamId: teamId)));
+    assert(!response.hasErrors, response.errors.toString());
+
+    List<TaskCompletion> taskCompletions_ = [
+      for (var completion in response.data!.completions!)
+        TaskCompletion.fromResp(completion!)
+    ];
+
+    if (listEquals<TaskCompletion>(taskCompletions, taskCompletions_)) {
+      return;
+    }
+
+    taskCompletions = taskCompletions_;
+    notifyListeners();
+  }
+
+  Future<CreateResult> createTask(String name, String? description, int points,
+      int teamId, String refreshInterval, bool isRecurring) async {
+    GraphQLResponse<CreateTask$Mutation> response = await _client.execute(
+        CreateTaskMutation(
+            variables: CreateTaskArguments(
+                input: CreateTaskInput(
+                    name: name,
+                    description: description,
+                    team: teamId.toString(),
+                    basePointsPrize: points,
+                    refreshInterval: refreshInterval,
+                    isRecurring: isRecurring))));
+
+    if (!response.hasErrors &&
+        (response.data!.createTask!.errors?.isEmpty ?? false)) {
+      tasks.add(Task.fromCreateResp(response.data!.createTask!.task!));
+      notifyListeners();
+    }
+
+    return CreateResult(response.hasErrors, response);
   }
 }
